f3ccb2c1a244b53568adbc2e0c095ff03d2466b5
Keith Flynn <flynn.keith@gmail.com>
2023-03-04 21:40:55 -0500

    sql_stuff.py in a good place before I add joins

--- sql_stuff.py
+++ sql_stuff.py
@@ -1,18 +1,43 @@
 import sqlite3
 import pandas as pd
-import sqlalchemy
 
 # dates 2009-10-28 - 2017-09-08
-start_date = '2009-10-28'
-end_date = '2017-09-08'
+# bite_date,SpeciesIDDesc,BreedIDDesc,GenderIDDesc,color,victim_zip,WhereBittenIDDesc,ResultsIDDesc
 
 con = sqlite3.connect("lunacy.db")
 
-bite_df = pd.read_sql("SELECT * FROM bites b WHERE b.bite_date BETWEEN '2009-10-29' and '2017-09-08' ORDER BY bite_date;", con, parse_dates='bite_date')
+bite_df = pd.read_sql("""SELECT 
+                            bite_date, 
+                            SpeciesIDDesc, 
+                            BreedIDDesc, 
+                            GenderIDDesc, 
+                            color, 
+                            victim_zip, 
+                            WhereBittenIDDesc, 
+                            ResultsIDDesc 
+                        FROM 
+                            bites b 
+                        WHERE 
+                            b.bite_date 
+                        BETWEEN 
+                            '2009-10-29' and '2017-09-08' 
+                        ORDER BY 
+                            bite_date;""", con)
 
 print(bite_df)
 
-moon_df = pd.read_sql("SELECT * FROM moon m WHERE m.date BETWEEN '2009-10-29' and '2017-09-08' ORDER BY date;", con, parse_dates='date')
+moon_df = pd.read_sql("""SELECT 
+                            date, 
+                            illum_pct, 
+                            phase 
+                        FROM 
+                            moon m 
+                        WHERE 
+                            m.date 
+                        BETWEEN 
+                            '2009-10-29' and '2017-09-08' 
+                        ORDER BY 
+                            date;""", con)
 
 print(moon_df)
 
